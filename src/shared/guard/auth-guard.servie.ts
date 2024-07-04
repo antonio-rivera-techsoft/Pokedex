951ee238377bfe03b588a674e8013157b9e660f5
Antonio Rivera <antonio.rivera@techsoft.com.mx>
2024-07-04 11:26:57 -0700

    Se agrego redireccion a login al entrar a pagina sin permiso

--- src/shared/guard/auth-guard.servie.ts
+++ src/shared/guard/auth-guard.servie.ts
@@ -1,16 +1,19 @@
 import { Injectable, inject } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuardService implements CanActivate{
-    private auth = inject(AuthService);
-    isLogged: boolean = false;
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  isLogged: boolean = false;
+
   canActivate():boolean{
     this.auth.isLogged().subscribe(l => this.isLogged = l);
     if(this.isLogged){
         return true;
     }
+    this.router.navigate(['/login']);
     return false;
   }
 }
